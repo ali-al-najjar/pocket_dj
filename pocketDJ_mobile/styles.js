523a481a0dc252a6c53f75e574d7e036cdd9e151
aliynajjar <ali.y.alnajjar@gmail.com>
2023-04-24 19:17:50 +0300

    Mobile: Restructured the folders by adding src folder and styles sheets

--- pocketDJ_mobile/styles.js
+++ pocketDJ_mobile/styles.js
@@ -1,14 +1,3 @@
-import { StyleSheet } from 'react-native';
-
-
-const primaryColor = '#FC6422';
-const secondaryColor = '#87F966';
-const tertiaryColor = '#FC1CFD';
-const textColor = '#000000';
-const white ='white';
-const black = '#1E1E1E';
-
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: white,
