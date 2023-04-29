f274ae121695e1d477ba82f3a8ce1f8d7ac6a0a8
aliynajjar <ali.y.alnajjar@gmail.com>
2023-04-29 21:49:17 +0300

    Frontend: Linked login page :white_check_mark:

--- pocketdj_frontend/src/components/LoginForm/loginform.jsx
+++ pocketdj_frontend/src/components/LoginForm/loginform.jsx
@@ -3,12 +3,17 @@ import  './loginform.css';
 import Button from "../Button/button";
 import {useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
+
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const[error,setError]=useState("");
 
+const navigator = useNavigate();
 const handleUsername=(e)=>{
       setUsername(e.target.value)
   }
@@ -30,19 +35,22 @@ const handleUsername=(e)=>{
             password: password
           };
           console.log(data)
-//     axios.post("http://127.0.0.1:8000/api/v0.0.1/auth/login",data).then((res) => {
-//         console.log(res)
-//         localStorage.setItem('token',res.data.authorisation.token);
-//         localStorage.setItem('email',res.data.user.email);
-//         window.location.href="http://localhost:3000/code_editor"  
-// }
-//     ).catch((err) => {
-//         console.log(err);
-//     })
-// }else(setError("Invalid credentials"))
-// }else(setError("Invalid credentials"))
+          axios.post("http://192.168.1.127:8000/login",data,{
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then((res)=>{
+            console.log(res.data);
+            setError("");
+            navigator("/artist/upload")
+          })
+          .catch((err=>{
+            console.log(err.request.response);
+          }))
+      }else(setError("Invalid Password"))
 
-  }}
+  }
  
     return(
       <>
