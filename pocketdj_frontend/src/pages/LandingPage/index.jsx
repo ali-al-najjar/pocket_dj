4af783e13c996ab4f85e13dfb3526903ee40459d
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-02 00:36:58 +0300

    Frontend: Fixed Css to tables :art:

--- pocketdj_frontend/src/pages/LandingPage/index.jsx
+++ pocketdj_frontend/src/pages/LandingPage/index.jsx
@@ -5,12 +5,12 @@ import "./index.css"
 const Landing = () => {
     return (
         <div className="landing_container">
-        <a className = "landing_left_box" href="">
+        <a className = "landing_left_box" href="/admin">
         <div className="landing_heading_box_left">
         <h1 className="landing_page_title_left">Admin Portal</h1>
         <h2 className="landing_heading_left">Log in to your account</h2>
         </div></a>
-        <a className="landing_right_box" href="">
+        <a className="landing_right_box" href="/artist">
         <div className="landing_heading_box_right">
         <h1 className="landing_page_title_right">Artist Portal</h1>
         <h2 className="landing_heading_right">Log in to your account</h2>
