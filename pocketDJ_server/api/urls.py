32f943c0a62414d31a5ac2184181025b4b08135e
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-02 16:04:13 +0300

    Backend: Added Delete Urls to all the views :green_heart:

--- pocketDJ_server/api/urls.py
+++ pocketDJ_server/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import UserDetails,RegisterUser,CreateSong,CreateMood,CreateRemix,CreateRequest,GetUsers,GetArtists,GetSongs,GetRemixes,GetRequests,GetMoods,DeleteSong
+from .views import UserDetails,RegisterUser,CreateSong,CreateMood,CreateRemix,CreateRequest,GetUsers,GetArtists,GetSongs,GetRemixes,GetRequests,GetMoods,DeleteSong,DeleteMood,DeleteRemix,DeleteRequest
 urlpatterns = [
   path('get-details',UserDetails.as_view()),
   path('register',RegisterUser.as_view()),
   path('song/create', CreateSong.as_view()),
   path('song/delete/<int:pk>',DeleteSong.as_view()),
   path('mood/create', CreateMood.as_view()),
+  path('mood/delete/<int:pk>',DeleteMood.as_view()),
   path('remix/create', CreateRemix.as_view()),
+  path('remix/delete/<int:pk>',DeleteRemix.as_view()),
   path('request/create', CreateRequest.as_view()),
+  path('request/delete/<int:pk>',DeleteRequest.as_view()),
   path('users/', GetUsers.as_view()),
   path('songs/', GetSongs.as_view()),
   path('remixes/', GetRemixes.as_view()),
