76598c26f9b904e6a13ceb6b5f29cacec778bd00
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-04 14:33:23 +0300

    Frontend: Updated profile pic url :ok_hand:

--- pocketDJ_mobile/src/screens/profile/ProfileScreen.js
+++ pocketDJ_mobile/src/screens/profile/ProfileScreen.js
@@ -10,7 +10,7 @@ const ProfileScreen = () => {
   return (
   <SafeAreaView style={styles.container}>
   <View style={styles.cover_container}>
-  <Image source={{uri: user.profile}}
+  <Image source={{uri:`http://192.168.1.127:8000/${user.profile}`}}
   style={styles.imageStyle}></Image>
   </View>
   <View style={styles.mood_title}>
