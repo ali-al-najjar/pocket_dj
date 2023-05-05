e6126314b76e980b67104473df671aa01aed455d
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-05 13:56:32 +0300

    Mobile: Created Song Player :green_heart:

---
+++ pocketDJ_mobile/src/components/SongPlayer/SongPlayer.js
@@ -0,0 +1,62 @@
+import { SliderComponent, Text, TouchableOpacity, View } from "react-native"
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
+import Button from "../../components/Button/Button";
+import Colors from "../../constants/colors";
+import Slider from '@react-native-community/slider'
+
+
+const AudioPlayer = () => {
+  const navigation = useNavigation();
+  const [isPlaying, setIsPlaying] = useState(false);
+  
+  const togglePlayer = () => {
+    setIsPlaying(!isPlaying);
+  }
+  
+  return (
+    <SafeAreaView style={styles.audioScreenContainer}>
+    <View style={styles.container}>
+    <View>
+      <Slider
+        style={styles.progressBar}
+        value={0}
+        minimumValue={0}
+        maximumValue={100}
+        thumbTintColor={Colors.secondaryColor}
+        minimumTrackTintColor={Colors.secondaryColor}
+        maximumTrackTintColor={Colors.black}
+        onSlidingComplete={()=>{}}
+       />
+    </View>
+    <View style={styles.progressBarCounter}>
+      <Text style={styles.progressBarCounterStart}>0:00</Text>
+      <Text style={styles.progressBarCounterStart}>3:50</Text>
+    </View>
+    <View style={styles.audio_player}>
+
+    <TouchableOpacity>
+    <Ionicons name="ios-play-back-circle-outline" size={70} color={Colors.black} />
+    </TouchableOpacity>
+    <TouchableOpacity onPress={togglePlayer}>
+      {isPlaying ? (
+            <Ionicons name="ios-pause-circle-outline" size={120} color={Colors.black} />
+          ) : (
+            <Ionicons name="ios-play-circle-outline" size={120} color={Colors.black} />
+          )}
+    </TouchableOpacity>
+    <TouchableOpacity>
+    <Ionicons name="ios-play-forward-circle-outline" size={70} color={Colors.black} />
+    </TouchableOpacity>
+    </View>
+    <Button title="Save" onPress={() => console.log("Your Remix is saved")} />
+    </View>
+    </SafeAreaView>
+  )
+}
+
+export default AudioPlayer;
--- pocketDJ_mobile/src/screens/search/SearchScreen.js
+++ pocketDJ_mobile/src/screens/search/SearchScreen.js
@@ -1,16 +1,66 @@
-import { View, Text, TextInput, TouchableOpacity, Image,SafeAreaView} from "react-native";
+import { View, Text,Image, FlatList} from "react-native";
 import styles from './styles';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { useNavigation } from "@react-navigation/native";
-import constants from '../../constants/styles';
+import MoodItem from "../../components/Moods/Moods/MoodItem";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getToken } from "../../auth/auth";
+import axios from 'axios';
+
+const renderItem = ({item}) => {
+  console.log("Rendering item with ID:", item.image);
+  return <MoodItem id={item.id} name={item.name} cover={item.cover} />
+}
 
 const SearchScreen = () => {
-  return (
-    <SafeAreaView>
-    <Text>This is the player page</Text>
-    </SafeAreaView>
+  const navigation = useNavigation();
+  const token = getToken();
+  const [responses, setResponses] = useState([]);
+
+  const getSongs = async () => {
+    try {
+      const res = await axios({
+        method: 'GET',
+        url: 'http://192.168.1.127:8000/songs',
+        headers: {
+          Authorization: 'Token ' + token,
+        },
+      });
+      setResponses(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getSongs();
+  }, []);
   
+  const header = () =>{
+    return (
+      <>
+      <SafeAreaView style={styles.topSafeArea} >
+      <Image 
+        source={require('../../../assets/mood.png')}
+        style={styles.innerImageContainer}
+      />
+      </SafeAreaView>
+      <View style={styles.h1_view}>
+      <Text style={styles.h1_text}>What are you up to?</Text>
+      </View>
+      </>
     )
+  }
+  return(
+    <FlatList 
+      ListHeaderComponent ={header}
+      style={styles.flatlist}
+      data= {responses}
+      keyExtractor={item => item.id}
+      renderItem = {renderItem}
+      numColumns={2}
+      />
+  )
 }
 
 export default SearchScreen;
