c429f9acab5a6f1058b3f9290036095d97070ed1
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-06 04:44:54 +0300

    Mobile: Linked Remixes page :construction:

--- pocketDJ_mobile/src/screens/remixes/RemixesScreen.js
+++ pocketDJ_mobile/src/screens/remixes/RemixesScreen.js
@@ -1,16 +1,52 @@
-import { View, Text,FlatList} from "react-native";
+import { View, Text,FlatList,RefreshControl} from "react-native";
 import styles from './styles';
 import { useNavigation } from "@react-navigation/native";
 import { DUMMY_DATA } from "../../../data/remixes";
 import RemixItem from "../../components/Remix/RemixItem";
 import { SafeAreaView } from "react-native-safe-area-context";
 import EmptyState from "../../components/EmptyState/emptyState";
+import { getToken } from "../../auth/auth";
+import axios from 'axios';
+import {useState, useEffect,useCallback} from 'react';
 
 const renderItem = ({item}) => {
   return <RemixItem id={item.id} title={item.title} mood={item.mood} date={item.date} />}
 
 const RemixesScreen = () => {
   const navigation = useNavigation();
+  const token = getToken();
+  const [responses, setResponses] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setSearch("")
+      setRefreshing(false);
+      getArtists();
+      getSongs();
+    }, 1000);
+  }, []);
+
+  const getRemixes = async () => {
+    try {
+      const res = await axios({
+        method: 'GET',
+        url: 'http://192.168.1.127:8000/remixes',
+        headers: {
+          Authorization: 'Token ' + token,
+        },
+      });
+      setResponses(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getRemixes();
+  }, []);
+
   const header = () =>{
     return (
     <View>
@@ -23,6 +59,8 @@ const RemixesScreen = () => {
   return(
     <>
     <FlatList 
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       style={styles.flatList}
       ListHeaderComponent ={header}
       ListEmptyComponent={
@@ -33,7 +71,7 @@ const RemixesScreen = () => {
         onPress={()=> {
           navigation.navigate("Create");}}
       />}
-      data= {DUMMY_DATA}
+      data= {responses}
       keyExtractor={item => item.id}
       renderItem = {renderItem}
       />
