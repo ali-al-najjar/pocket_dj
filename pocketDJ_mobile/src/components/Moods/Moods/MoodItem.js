2e912e9367b17e5a546c6014f178d6cc6a9e9bfb
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-08 04:12:46 +0300

    Mobile: Linked moods to create new remixes :tada:

--- pocketDJ_mobile/src/components/Moods/Moods/MoodItem.js
+++ pocketDJ_mobile/src/components/Moods/Moods/MoodItem.js
@@ -1,21 +1,41 @@
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native"
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
-
-
+import { getToken } from "../../../auth/auth";
+import axios from 'axios';
 
 const MoodItem = ({id , name ,cover}) => {
   const navigation = useNavigation();
 
-  const handleMood=()=>{
-    navigation.navigate('Player', {
-      title: name,
-      image: { uri: cover },
-      });
-  }
+  // const handleMood=()=>{
+  //   navigation.navigate('Player', {
+  //     title: name,
+  //     image: { uri: cover },
+  //     });
+  // }
+  const token = getToken();
   
+  const data = new FormData();
+  data.append('mood_id', id)
+  const MakeRemix = async () => {
+    axios.post("http://192.168.1.127:8000/mix",data,{
+      headers: {
+        Authorization: 'Token ' + token._j,
+        "Content-Type": "multipart/form-data"
+      }
+    })
+    .then((res)=>{
+      console.log(res.data);
+      
+    })
+    .catch((err=>{
+      console.log(err.request.response);
+      console.log(token)
+    }))
+  }
+
   return (
-    <TouchableOpacity onPress={handleMood}>
+    <TouchableOpacity onPress={MakeRemix}>
     <ImageBackground 
     source={{ uri: cover }}
     imageStyle={{ borderRadius: 10}}
