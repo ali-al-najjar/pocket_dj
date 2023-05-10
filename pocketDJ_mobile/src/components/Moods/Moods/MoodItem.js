f1b4b3102b6b33360ec87a2cebba8f324db68d56
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-10 03:24:56 +0300

    Mobile: Linked Moods to song Player:art:

--- pocketDJ_mobile/src/components/Moods/Moods/MoodItem.js
+++ pocketDJ_mobile/src/components/Moods/Moods/MoodItem.js
@@ -1,80 +1,76 @@
-import { ImageBackground, Text, TouchableOpacity, View ,ActivityIndicator} from "react-native"
-import {useState, useEffect,useCallback} from 'react';
+import { ImageBackground, Text, TouchableOpacity, View, ActivityIndicator } from "react-native"
+import { useState,useEffect } from 'react';
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { getToken } from "../../../auth/auth";
 import axios from 'axios';
-// import RemixItem from "../../components/Remix/RemixItem";
 import { Alert } from 'react-native';
 import Colors from "../../../constants/colors";
+import SongItem from "../../Songs/SongItem";
 
-const MoodItem = ({id , name ,cover}) => {
+const MoodItem = ({ id, name, cover }) => {
 
   const navigation = useNavigation();
-  // const [loading, setLoading] = useState(true);
-  // const [remix, setRemix] = useState();
-  // const handleMood=()=>{
-  //   navigation.navigate('Player', {
-  //     title: name,
-  //     image: { uri: cover },
-  //     });
-  // }
+  const [showIndicator, setShowIndicator] = useState(false);
+  const [mix, setMix] = useState();
   const token = getToken();
-  
+
   const data = new FormData();
   data.append('mood_id', id)
+
   const MakeRemix = async () => {
-    axios.post("http://192.168.1.127:8000/mix",data,{
+    setShowIndicator(true);
+    axios.post("http://192.168.1.127:8000/mix", data, {
       headers: {
         Authorization: 'Token ' + token._j,
         "Content-Type": "multipart/form-data"
       }
     })
-    .then((res)=>{
-      // setLoading(false);
-      console.log(res.status)
-      console.log(res.data);
-      
-    })
-    .catch((err=>{
-      // setLoading(false);
-      console.log(err.request.response);
-      Alert.alert(
-        'Unlucky!',
-        'New Songs will be added soon for this item',
-        [
-          {
-            text: 'OK',
-            onPress: () => console.log('OK pressed'),
-            style: 'cancel'
-          }
-        ],{
-        backgroundColor: Colors.rgbaPrimary,
-        color: 'black',
-        titleStyle: {
-          fontSize: 24,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'red'
-        },
-        messageStyle: {
-          fontSize: 16,
-          textAlign: 'center',
-          color: 'black'
-        }
-      });
-    }))
+      .then((res) => {
+        setShowIndicator(false);
+        if (res.status === 200) {
+          console.log(res.data);
+          setMix(res.data)
+      }})
+      .catch((err => {
+        setShowIndicator(false);
+        console.log(err.request.response);
+        Alert.alert(
+          'Unlucky!',
+          'New Songs will be added soon for this item',
+          [
+            {
+              text: 'Choose Another Mood',
+              onPress: () => console.log('OK pressed'),
+              style: 'destructive'
+            }
+          ]);
+      }))
   }
+
+  useEffect(() => {
+    if (mix) {
+      const durationinMillis = Math.ceil(mix.duration * 60000);
+      const audio = mix.link;
+      const cover = mix.cover;
+      navigation.navigate('Song Player', {
+        title: mix.name,
+        image: cover,
+        AudioURL: audio,
+        Duration: durationinMillis,
+      });
+    }
+  }, [mix]);
 
   return (
     <TouchableOpacity onPress={MakeRemix}>
-    <ImageBackground 
-    source={{ uri: cover }}
-    imageStyle={{ borderRadius: 10}}
-    style = {[styles.mood, { borderRadius: 5 }]}>
-    <Text style={styles.mood_text}>{name}</Text>
-    </ImageBackground>
-    {/* {loading ? <ActivityIndicator size="large" color="#0000ff" /> : <RemixItem id={remix.id} title={remix.name} date={remix.date} mood={item.mood_name} audio={item.link} cover={item.cover} duration={parseFloat(item.duration)}/>} */}
+      <ImageBackground
+        source={{ uri: cover }}
+        imageStyle={{ borderRadius: 10 }}
+        style={[styles.mood, { borderRadius: 5 }]}>
+        <Text style={styles.mood_text}>{name}</Text>
+        {showIndicator ? <ActivityIndicator size="large" color="#0000ff" /> : null}
+      </ImageBackground>
     </TouchableOpacity>
   )
 }
