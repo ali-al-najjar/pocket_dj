2263bb3232f4604b3995086877fa985dfcff1e7b
aliynajjar <ali.y.alnajjar@gmail.com>
2023-05-11 07:05:33 +0300

    Mobile: Added Keyboard respect :ambulance:

--- pocketDJ_mobile/App.js
+++ pocketDJ_mobile/App.js
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import {store} from './src/redux/store'
 import { Provider } from 'react-redux'
 import { LogBox } from 'react-native';
-
+import { KeyboardAvoidingView } from 'react-native';
 
 
 export default function App() {
@@ -14,10 +14,10 @@ export default function App() {
     <Provider store={store}>
     <NavigationContainer>
       <StatusBar style = "dark" ></StatusBar>
+      <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
       <HomeStack></HomeStack>
+      </KeyboardAvoidingView>
     </NavigationContainer>
     </Provider>
   );
 }
-
-
